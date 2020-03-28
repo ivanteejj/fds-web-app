4911d500153dbd3f1713cde1e14ecde6f537c9ab
ivanteejj <58815457+ivanteejj@users.noreply.github.com>
2020-03-29 04:37:46 +0800

    added basic customer pages

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,11 @@ import logo from './logo.svg'
 import './App.css'
 
 //Pages
-import SignupPage from "./pages/signup"
+import RegisterPage from "./pages/register"
 import LoginPage from "./pages/login"
 import MainPage from "./pages"
 import NotFoundPage from "./pages/404"
-
+import CustomerHomePage from "./pages/customer/cust_home"
 import Home from './components/customers/home/homepage'
 //import Login from './components/customers/account/login'
 import Signup from './components/customers/account/signup'
@@ -30,7 +30,8 @@ class App extends Component {
             <Route exact path="/" component = { MainPage }></Route>
             <Route exact path = "/404" component = { NotFoundPage }></Route>
             <Route exact path = "/login" component = { LoginPage }></Route>
-            <Route exact path = "/signup" component = { SignupPage }></Route>
+            <Route exact path = "/register" component = { RegisterPage }></Route>
+            <Route path = "/customer" component = { CustomerHomePage }></Route>
             <Redirect to = "/404"></Redirect>
             </Switch>
         </Router>
