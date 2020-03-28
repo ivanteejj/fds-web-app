0d40501205d4604234340310ba399638e6d2e1c3
ivanteejj <58815457+ivanteejj@users.noreply.github.com>
2020-03-29 01:46:00 +0800

    added basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react'
+import { BrowserRouter as Router, 
+    Route, 
+    Switch, 
+    Link, 
+    Redirect 
+} from "react-router-dom"
 import logo from './logo.svg'
 import './App.css'
+
+//Pages
+import SignupPage from "./pages/signup"
+import LoginPage from "./pages/login"
+import MainPage from "./pages"
+import NotFoundPage from "./pages/404"
+
 import Home from './components/customers/home/homepage'
-import Login from './components/customers/account/login'
+//import Login from './components/customers/account/login'
 import Signup from './components/customers/account/signup'
 
 class App extends Component {
@@ -11,27 +24,16 @@ class App extends Component {
         this.state = { apiResponse: " "};
     }
 
-    callAPI() {
-        fetch("http://localhost:9000/testAPI")
-            .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res}))
-            .catch(err => err);
-    }
-
-    componentDidMount() {
-        this.callAPI();
-    }
-
     render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <h1>FDS</h1>
-                </header>
-                <Login />
-                <p className = "App-intro"> { this.state.apiResponse }</p>
-            </div>
-        );
+        return <Router>
+            <Switch>
+            <Route exact path="/" component = { MainPage }></Route>
+            <Route exact path = "/404" component = { NotFoundPage }></Route>
+            <Route exact path = "/login" component = { LoginPage }></Route>
+            <Route exact path = "/signup" component = { SignupPage }></Route>
+            <Redirect to = "/404"></Redirect>
+            </Switch>
+        </Router>
     }
 }
 
