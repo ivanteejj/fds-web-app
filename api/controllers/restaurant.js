f113cde21d2432d5a2ec0640d65787cf6be4e35c
ivanteejj <58815457+ivanteejj@users.noreply.github.com>
2020-04-01 19:32:03 +0800

    updated routing

--- api/controllers/restaurant.js
+++ api/controllers/restaurant.js
@@ -16,7 +16,21 @@ const getOneRestDetails = (req, res, db) => {
         })
     }
 
+
+const queryForGetAllRestDataForHomePage = "SELECT r1.rname, r1.rest_id as rid, r1.area, r1.address, f1.fname, f1.fid, f1.price, f1.category, f1.daily_limit FROM Restaurants r1 JOIN food f1 ON r1.rest_id = f1.rest_id"
+const getAllRestDataForHomePage = (req, res, db) => {
+    const output = db.query(queryForGetAllRestDataForHomePage,
+        (error,  results) => {
+            if (error) {
+                console.log(error)
+            }
+
+            res.status(200).json(results.rows)
+        })
+    }    
+
 module.exports = {
     getOneRestDetails: getOneRestDetails,
+    getAllRestDataForHomePage: getAllRestDataForHomePage,
 };
 
